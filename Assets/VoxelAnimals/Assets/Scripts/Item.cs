374720c07d713238ddaae99e73344996a8d5f014
Katteri <66689996+Katteri@users.noreply.github.com>
2023-10-19 10:58:22 +0300

    pr2-3

---
+++ Assets/VoxelAnimals/Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Item
+{
+    public string Title;
+    public GameObject buttonPrefab;
+    public GameObject objectPrefab;
+    public int Price;
+}
+
